9b6b9a67cc84fb8118a0d16430f23ccccdcbd320
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-25 00:06:36 +0700

    config dbcontext

---
+++ HotelListing/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HotelListing.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+    }
+}
