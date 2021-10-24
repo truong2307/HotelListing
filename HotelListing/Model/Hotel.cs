f0894ba01498b5ef53100d736485d3cd1ad04b64
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-25 00:06:23 +0700

    country, hotel model, EF

---
+++ HotelListing/Model/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Model
+{
+    public class Hotel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Double Rating { get; set; }
+        public int CountryId { get; set; }
+
+        [ForeignKey("CountryId")]
+        public Country Country { get; set; }
+    }
+}
