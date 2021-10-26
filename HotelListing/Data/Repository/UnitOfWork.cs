580cdffc293890298817379f90a3115b72662646
Vantruongg <toiiuvn013@gmail.com>
2021-10-27 01:44:55 +0700

    UnitOfWork

---
+++ HotelListing/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using HotelListing.Data.Repository.IRepository;
+using HotelListing.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Country> Countries => _countries ?? new GenericRepository<Country>(_context);
+
+        public IGenericRepository<Hotel> Hotels => _hotels ?? new GenericRepository<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
