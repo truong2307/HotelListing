bec54860fc1bc011a5481871a63ccf1d0a4f0ca0
Vantruongg <toiiuvn013@gmail.com>
2021-10-27 01:23:27 +0700

    Repository generic

---
+++ HotelListing/Data/Repository/IRepository/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data.Repository.IRepository
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<IList<T>> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null
+            );
+        Task<T> Get(Expression<Func<T, bool>> filter, List<string> includes = null);
+        Task Insert(T entity);
+        Task InsertRange(IEnumerable<T> entities);
+        Task Delete(int id);
+        void DeleteRange(IEnumerable<T> entities);
+        void Update(T entity);
+    }
+}
