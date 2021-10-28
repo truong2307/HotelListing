86ef101aae09664a5034750084aa1f3fad2f3db6
Vantruongg <toiiuvn013@gmail.com>
2021-10-29 01:25:47 +0700

    config newtonsoft

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -51,7 +51,9 @@ namespace HotelListing
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = 
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
