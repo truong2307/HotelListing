43d9d1d5788b72d6ad80f2566842c7d6bbb56496
Vantruongg <toiiuvn013@gmail.com>
2021-10-30 00:44:54 +0700

    indetity ef

--- HotelListing/Data/Migrations/20211029174259_addIdentityCore.Designer.cs
+++ HotelListing/Data/Migrations/20211029174259_addIdentityCore.Designer.cs
@@ -4,14 +4,16 @@ using HotelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelListing.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211029174259_addIdentityCore")]
+    partial class addIdentityCore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
