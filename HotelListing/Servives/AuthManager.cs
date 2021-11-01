daffb112b07e54f4d42145e44aeef8a18b7b0bd7
Vantruongg <toiiuvn013@gmail.com>
2021-11-02 01:59:52 +0700

    validate user, create token

---
+++ HotelListing/Servives/AuthManager.cs
@@ -0,0 +1,91 @@
+﻿using HotelListing.Dto;
+using HotelListing.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelListing.Servives
+{
+    public class AuthManager : IAuthManager
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private ApiUser _user;
+
+        public AuthManager(UserManager<ApiUser> userManager
+            , IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+        public async Task<string> CreateToken()
+        {
+            var singingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var token = GenerateTokenOptions(claims, singingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(List<Claim> claims, SigningCredentials singingCredentials)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var expiration = DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings.GetSection("lifetime").Value));
+
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings.GetSection("Issuer").Value,
+                claims: claims,
+                expires: expiration,
+                signingCredentials: singingCredentials
+                );
+
+            return token;
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Environment.GetEnvironmentVariable("KEY");
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var singingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+
+            return singingCredentials;
+        }
+
+        public async Task<bool> ValidateUser(LoginUserDto userRequest)
+        {
+            _user = await _userManager.FindByNameAsync(userRequest.Email);
+            bool checkUserSuccess = await _userManager.CheckPasswordAsync(_user, userRequest.Password);
+
+            if (_user != null && checkUserSuccess)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ HotelListing/Servives/IAuthManager.cs
@@ -0,0 +1,14 @@
+﻿using HotelListing.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Servives
+{
+    public interface IAuthManager
+    {
+        Task<bool> ValidateUser(LoginUserDto userRequest);
+        Task<string> CreateToken();
+    }
+}
